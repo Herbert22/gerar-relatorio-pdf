509662c2abdca09f01538378d13e5cdb94be0285
Herbert Carvalho <betoo.carvalho.167@gmail.com>
2024-06-17 08:55:41 -0300

    colocando o projeto no github

---
+++ src/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeradorRelatoriosPDF
+{
+    [Serializable]
+    class Pessoa
+    {
+        public int IdPessoa { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public double Salario { get; set; }
+
+        public Profissao Profissao { get; set; }
+
+        public bool Empregado { get; set; }
+    }
+}
